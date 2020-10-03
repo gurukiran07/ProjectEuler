0d5d53e8d0c7ca96b4cb46b0d8cd5d9cda1cea3e
guru kiran <47276342+gurukiran07@users.noreply.github.com>
2020-10-03 16:21:58 +0530

    replaced int with size_t

--- PE#1/PE#1.cpp
+++ PE#1/PE#1.cpp
@@ -7,8 +7,8 @@ using namespace std;
  */
 
 int main() {
-    int sum=0;
-    for(int i=0; i<1000; ++i){
+    size_t sum=0;
+    for(size_t i=0; i<1000; ++i){
         if (i%3==0 || i%5==0){
             sum = sum+i;
         }
