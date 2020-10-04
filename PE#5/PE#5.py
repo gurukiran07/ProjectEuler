e38bbdb145b8e9aa3b67063ab9fbfeb2de05e319
guru kiran <47276342+gurukiran07@users.noreply.github.com>
2020-10-04 23:23:10 +0530

    Updated with integer division `//`

--- PE#5/PE#5.py
+++ PE#5/PE#5.py
@@ -5,12 +5,12 @@ from math import gcd
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 """
 
-lcm = lambda a,b: a*b/gcd(int(a),int(b))
+lcm = lambda a,b: a*b//gcd(int(a),int(b))
 
 def smallest_multiple(n):
   val = 2
   for i in range(3,n+1):
     val = lcm(val, i)
-  return val # return type is float, as gcd return a float. To get int use `return int(val)`
+  return val
   
 smallest_multiple(20)  
