943b3e1fd90a675284b899b75c84d99157856407
guru kiran <47276342+gurukiran07@users.noreply.github.com>
2020-10-06 16:28:35 +0530

    PE#&7 cpp file

---
+++ PE#7/PE#7.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+/*
+ * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+ * What is the 10 001st prime number?
+ */
+
+bool isPrime(uint64_t num){
+	if (num==2 || num==3)
+		return true;
+	if (num==1 || num%2==0 || num%3==0)
+		return false;
+
+	for(size_t i=5; i*i<=num;++i){
+		if(num%i==0)return false;}
+	return true;
+}
+	
+uint64_t Prime10_001(){
+	uint64_t prime=3;
+	size_t count=2;
+	size_t i=5;
+	while(count!=10001){
+		if(isPrime(i)){
+			++count;
+			prime = i;
+		}
+		++i;
+	}
+	return prime;
+}
+int main(){
+	cout<<Prime10_001();
+	return 0;
+}
