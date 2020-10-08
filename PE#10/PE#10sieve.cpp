20e0380c4c6ea4436fd2433e3e35df3bd0a0438d
guru kiran <47276342+gurukiran07@users.noreply.github.com>
2020-10-08 19:43:45 +0530

    PE#10 sieve of Eratosthenes

---
+++ PE#10/PE#10sieve.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void PrimeSum(){
+	vector<bool>prime(2e6,true);
+	for(size_t i=2; i<2e6; ++i){
+		if(prime[i]){
+			for(size_t j=i*i; j<2e6; j=j+i)
+				prime[j]=false;
+			}
+	}
+	
+	uint64_t sum=0;
+	for(size_t i=2; i<2e6; ++i){
+		if(prime[i])sum=sum+i;
+	}
+	cout<<sum<<endl;
+}
+
+int main(){
+	PrimeSum();
+}
+		
