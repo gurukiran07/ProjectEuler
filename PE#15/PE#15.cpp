c62e1906a9705da30d08c3ef441f5370dee68b24
guru kiran <47276342+gurukiran07@users.noreply.github.com>
2020-10-31 23:00:29 +0530

    LatticePaths cpp file

---
+++ PE#15/PE#15.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+// Reference : https://www.mathblog.dk/project-euler-15/
+
+uint64_t LatticePaths(){
+	int size = 20;
+	uint64_t out = 1;
+	for(size_t i=1; i<=size; ++i){
+		out *= size+i;
+		out /= i;
+	}
+	return out;
+}
+
+int main(){
+	cout<<LatticePaths();
+	return 0;
+}
